446b14019b166f3d4d2bfaa0290e17e01706303c
halterman <halterman@southern.edu>
2018-08-22 15:16:05 -0400

    Version: Wed Aug 22 15:15:53 USEDT 2018

---
+++ 2018-08-22-VariablePlay/variableplay.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+	std::cout << "This is a text message\n";
+	std::cout << "42" << '\n';
+	std::cout << 55 << '\n';
+
+	int x;
+	x = 100;
+	std::cout << "The variable's value is " << x << '\n';
+	int a = 3;
+	int b = 5;
+	std::cout << a + b << '\n';
+
+	int c = 12, d = 9.8;
+	std::cout << c + d << '\n';
+}
---
+++ InputTest2/InputTest2.cpp
@@ -0,0 +1,11 @@
+// InputTest2.cpp
+// Created 8/22/2018 2:56:11 PM
+
+#include <iostream>
+
+int main() {
+	int number;
+	std::cout << "Please enter an integer: ";
+	std::cin >> number;
+	std::cout << "You entered " << number << '\n';
+}
