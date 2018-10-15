9ce7322e4fdcdcb4d4bccb89b34acfe9d51510bb
halterman <halterman@southern.edu>
2018-10-15 13:53:50 -0400

    Version: Mon Oct 15 13:53:50 USEDT 2018

---
+++ averagenumbers/averagenumbers.cpp
@@ -0,0 +1,13 @@
+// averagenumbers.cpp
+// Created 10/15/2018 1:51:30 PM
+
+#include <iostream>
+int main() {
+	double n1, n2, n3, n4, n5;
+	std::cout << "Please enter five numbers: ";
+	// Allow the user to enter in the five values.
+	std::cin >> n1 >> n2 >> n3 >> n4 >> n5;
+	std::cout << "The average of " << n1 << ", " << n2 << ", "
+		<< n3 << ", " << n4 << ", " << n5 << " is "
+		<< (n1 + n2 + n3 + n4 + n5) / 5 << '\n';
+}
