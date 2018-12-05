2e201e531680fc98b926a96c7d4ac754a8b3382d
halterman <halterman@southern.edu>
2018-12-05 13:57:47 -0500

    Version: Wed Dec  5 13:57:46 USEST 2018

--- Quiz10/Quiz10.cpp
+++ Quiz10/Quiz10.cpp
@@ -1,17 +1,50 @@
-#include <GL/sgl.hpp>
+#include <iostream>
 
-class Shape {
+class Counter {
+	int count;
+	const int limit;
 public:
-	virtual void draw() const {
-
+	Counter(int limit) : count(0), limit(limit) {}
+	virtual void increment() {
+		if (count < limit)
+			count++;
+	}
+	int get() const {
+		return count;
 	}
 };
 
-void key_pressed(int key, double, double) {
-	
-}
-
+class ChimingCounter : public Counter {
+public:
+	ChimingCounter(int limit) : Counter(limit) {}
+	void increment() override {
+		Counter::increment();
+		std::cout << '\a';
+	}
+};
 
 int main() {
+	Counter ctr{ 3 };
+	std::cout << ctr.get() << '\n';  // Prints 0
+	ctr.increment();                 // Silence
+	std::cout << ctr.get() << '\n';  // Prints 1
+	ctr.increment();                 // Silence
+	std::cout << ctr.get() << '\n';  // Prints 2
+	ctr.increment();                 // Silence
+	std::cout << ctr.get() << '\n';  // Prints 3
+	ctr.increment();                 // Silence
+	std::cout << ctr.get() << '\n';  // Prints 3
+	ctr.increment();                 // Silence
+	std::cout << ctr.get() << '\n';  // Prints 3
 
+	ChimingCounter cc{ 2 };
+	std::cout << cc.get() << '\n';   // Prints 0
+	cc.increment();                  // Chimes
+	std::cout << cc.get() << '\n';   // Prints 1
+	cc.increment();                  // Chimes
+	std::cout << cc.get() << '\n';   // Prints 2
+	cc.increment();                  // Chimes
+	std::cout << cc.get() << '\n';   // Prints 2
+	cc.increment();                  // Chimes
+	std::cout << cc.get() << '\n';   // Prints 2
 }
